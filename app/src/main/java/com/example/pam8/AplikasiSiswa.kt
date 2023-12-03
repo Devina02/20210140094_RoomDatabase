434ce2b61cbb01cf773fbb7daff65ee7c6c94084
Devina02 <devinafitriapngs@gmail.com>
2023-12-03 22:43:55 +0700

    Menambahkan override

--- app/src/main/java/com/example/pam8/AplikasiSiswa.kt
+++ app/src/main/java/com/example/pam8/AplikasiSiswa.kt
@@ -1,2 +1,17 @@
 package com.example.pam8
 
+import android.app.Application
+import com.example.pam8.repositori.ContainerApp
+import com.example.pam8.repositori.ContainerDataApp
+
+class AplikasiSiswa : Application(){
+    /*
+    AppContainer instance digunakan oleh kelas-kelas lainnnya untuk mendapatkan dependensi
+     */
+    lateinit var container: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
+}
