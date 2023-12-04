f25626b4c86f9e25965685b068a2f57516b4bf7c
Devina02 <devinafitriapngs@gmail.com>
2023-12-04 12:17:15 +0700

    Membuat object PenyediaViewModel dan fungsi ekstensi query untuk objek

--- app/src/main/java/com/example/pam8/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pam8/model/PenyediaViewModel.kt
@@ -1,2 +1,23 @@
 package com.example.pam8.model
 
+import android.text.Spannable.Factory
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.pam8.AplikasiSiswa
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+    }
+}
+/**
+ * Fungsi ekstensi query untuk objek [Application] dan mengembalikann sebuah instance dari [AplikasiSiswa].
+ */
+fun CreationExtras.aplikasiSiswa():AplikasiSiswa = (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
