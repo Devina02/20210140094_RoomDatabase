07a970f175803ce8778c70c756ef26263b02f8e3
Devina02 <devinafitriapngs@gmail.com>
2023-12-05 17:34:43 +0700

    Membuat fun Homescreen

--- app/src/main/java/com/example/pam8/halaman/HalamanHome.kt
+++ app/src/main/java/com/example/pam8/halaman/HalamanHome.kt
@@ -1,2 +1,40 @@
 package com.example.pam8.halaman
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pam8.R
+import com.example.pam8.model.HomeViewModel
+import com.example.pam8.model.PenyediaViewModel
+
+object DestinasiHome : DestinasiHome{
+    override val route = "home"
+    override val titleRest = R.string.app_name
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    navigateToItemEntry: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold (
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            SiswaToAppBar(
+                title = stringResource(DestinasiHome.titleRest),
+                canNavigateBack = false,
+                scrollBehavior =  scrollBehavior
+            )
+        },
+
+    )
+}
