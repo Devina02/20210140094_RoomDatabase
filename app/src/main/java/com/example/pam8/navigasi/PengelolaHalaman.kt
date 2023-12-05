c07f1dc863cd498c8be67e1a111bd1065fc0a35f
Devina02 <devinafitriapngs@gmail.com>
2023-12-05 19:04:47 +0700

    menambahkan composable DestinasiHome dan DestinasiEntry

--- app/src/main/java/com/example/pam8/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/pam8/navigasi/PengelolaHalaman.kt
@@ -3,7 +3,13 @@ package com.example.pam8.navigasi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.pam8.halaman.DestinasiEntry
+import com.example.pam8.halaman.DestinasiHome
+import com.example.pam8.halaman.EntrySiswaScreen
+import com.example.pam8.halaman.HomeScreen
 
 @Composable
 fun SiswaApp(navController: NavController= rememberNavController()){
@@ -13,6 +19,18 @@ fun SiswaApp(navController: NavController= rememberNavController()){
 @Composable
 fun HostNavigasi(
     navController: NavController,
-    modifier: Modifier = Modifier) {
-
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController=navController, startDestination = DestinasiHome.route, modifier = Modifier)
+    {
+        composable(DestinasiHome.route){
+            HomeScreen(
+                navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
+                )
+        }
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(navigateBack = {navController.popBackStack()})
+        }
+    }
 }
