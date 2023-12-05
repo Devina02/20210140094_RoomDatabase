c6083362d15eb2bbebb69636051cd691facc991e
Devina02 <devinafitriapngs@gmail.com>
2023-12-05 18:54:47 +0700

    menambahkan fun ListSiswa dan DataSiswa

--- app/src/main/java/com/example/pam8/halaman/HalamanHome.kt
+++ app/src/main/java/com/example/pam8/halaman/HalamanHome.kt
@@ -1,15 +1,26 @@
 package com.example.pam8.halaman
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -19,13 +30,17 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pam8.R
 import com.example.pam8.data.Siswa
 import com.example.pam8.model.HomeViewModel
 import com.example.pam8.model.PenyediaViewModel
+import com.example.pam8.navigasi.DestinasiNavigasi
 
-object DestinasiHome : DestinasiHome{
+
+object DestinasiHome : DestinasiNavigasi{
     override val route = "home"
     override val titleRest = R.string.app_name
 }
@@ -80,6 +95,70 @@ fun BodyHome(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier
     ){
+        if (itemSiswa.isEmpty()){
+            Text(
+                text = stringResource(R.string.deskripsi_no_item),
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.titleLarge
+                )
+        } else {
+            ListSiswa(
+                ItemSiswa = itemSiswa,
+                modifier = Modifier.padding(horizontal = dimensionResource(id = R.dimen.padding_small))
+            )
+        }
+    }
+}
+@Composable
+fun ListSiswa(
+    ItemSiswa: List<Siswa>,
+    modifier: Modifier=Modifier
+) {
+    LazyColumn(modifier = Modifier){
+        items(items = itemSiswa, key = {it.id}){
+            person ->
+            DataSiswa(
+                siswa = person,
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_small))
+            )
+        }
+    }
+}
 
+@Composable
+fun DataSiswa(
+    siswa: Siswa,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = Modifier,
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+    ){
+        Column (
+            modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large)),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))
+        ){
+            Row (
+                modifier = Modifier.fillMaxWidth()
+            ){
+                Text(
+                    text = siswa.nama,
+                    style = MaterialTheme.typography.titleLarge
+                    )
+                Spacer(Modifier.weight(1f))
+                Icon(
+                    imageVector = Icons.Default.Phone,
+                    contentDescription = null,
+                    )
+                Text(
+                    text = siswa.telpon,
+                    style = MaterialTheme.typography.titleMedium
+                    )
+            }
+            Text(
+                text = siswa.alamat,
+                style = MaterialTheme.typography.titleMedium
+                )
+        }
     }
 }
