299f01d48ab76f2c47f140c62b9d3caad26bf098
Devina02 <devinafitriapngs@gmail.com>
2023-12-05 18:58:24 +0700

    membuat fun SiswaApp di PengelolaHalaman

--- app/src/main/java/com/example/pam8/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/pam8/navigasi/PengelolaHalaman.kt
@@ -1,2 +1,10 @@
 package com.example.pam8.navigasi
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun SiswaApp(navController: NavController= rememberNavController()){
+
+}
