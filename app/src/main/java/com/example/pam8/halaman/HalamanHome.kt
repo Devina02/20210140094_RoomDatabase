c9fa137d3e2d1856caf51e32c99d00b7842d5347
Devina02 <devinafitriapngs@gmail.com>
2023-12-05 17:38:44 +0700

    menambahkan ActionButton

--- app/src/main/java/com/example/pam8/halaman/HalamanHome.kt
+++ app/src/main/java/com/example/pam8/halaman/HalamanHome.kt
@@ -1,11 +1,18 @@
 package com.example.pam8.halaman
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pam8.R
@@ -35,6 +42,19 @@ fun HomeScreen(
                 scrollBehavior =  scrollBehavior
             )
         },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large))
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Add,
+                        contentDescription = stringResource(R.string.entry_siswa)
+                    )
+            }
+        }
+    ){
 
-    )
+    }
 }
